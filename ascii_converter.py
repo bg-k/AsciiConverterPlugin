4f4c927124706b2a6c10bb420330b97ca194a8b3
chan_gami <babyryo@msn.com>
2015-05-24 16:49:01 -0700

    :maple_leaf: Implement convert ASCII to HexaDecimal

--- ascii_converter.py
+++ ascii_converter.py
@@ -18,3 +18,18 @@ class AsciiToDecimalCommand(sublime_plugin.TextCommand):
 					result_string += str(ord(char))
 
 				self.view.replace(edit, selected_word, result_string)
+
+class AsciiToHexaDecimalCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		for selected_word in self.view.sel():
+			if not selected_word.empty():
+				target_string = self.view.substr(selected_word)
+				char_list = list(target_string)
+
+				result_string = ""
+				for char in char_list:
+					if not result_string == "":
+						result_string += " "
+					result_string += str(hex(ord(char)))
+
+				self.view.replace(edit, selected_word, result_string)
